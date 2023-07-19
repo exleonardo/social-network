1dbd2b1bdf4009b901140c5b48c7d5f78f4a0af9
Exleonardo <exleonardo@mail.ru>
2023-07-19 21:27:50 +0300

    Lesson 22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import s from "./Dialogs.module.css"
+import { NavLink } from "react-router-dom";
 
 const Dialogs = () => {
   return (
     <div className={ s.dialogs }>
       <div className={ s.dialogsItems }>
-        <div className={s.dialog + " " + s.active} >Dimych</div>
-        <div className={s.dialog}>Andrey</div>
-        <div className={s.dialog}>Svaeta</div>
-        <div className={s.dialog}>Victor</div>
-        <div className={s.dialog}>Valera</div>
+        <div className={s.dialog}><NavLink activeClassName={s.active}  to="/dialogs/1">Dimych</NavLink></div>
+        <div className={s.dialog}><NavLink activeClassName={s.active} to="/dialogs/2">Andrey</NavLink></div>
+        <div className={s.dialog}><NavLink activeClassName={s.active} to="/dialogs/3">Svaeta</NavLink></div>
+        <div className={s.dialog}><NavLink activeClassName={s.active} to="/dialogs/4">Victor</NavLink></div>
+        <div className={s.dialog}><NavLink activeClassName={s.active} to="/dialogs/5">Valera</NavLink></div>
       </div>
       <div className={s.messages} >
         <div className={ s.message }>HI</div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={style}><NavLink activeClassName={s.active} to="/profile">Profile</NavLink ></div>
-      <div className={s.item}><NavLink  activeClassName={s.active} to="/dialogs">Message</NavLink></div>
-      <div className={s.item}><NavLink  activeClassName={s.active} to="/news">News</NavLink></div>
+      <div className={s.item}><NavLink activeClassName={s.active} to="/dialogs">Message</NavLink></div>
+      <div className={s.item}><NavLink activeClassName={s.active} to="/news">News</NavLink></div>
       <div className={s.item}><NavLink activeClassName={s.active} to="/music">Music</NavLink></div>
       <div className={s.item}><NavLink activeClassName={s.active} to="/setting">Settings</NavLink></div>
     </nav>
