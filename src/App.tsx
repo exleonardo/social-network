fd239be149d9020c61d167bf6e7b8e8c8059608d
Exleonardo <exleonardo@mail.ru>
2023-07-26 17:10:38 +0300

    Lesson 27

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
@@ -10,25 +10,24 @@ import Setting from "./components/Setting/Setting";
 import Music from "./components/Music/Music";
 
 
-function App() {
+export type AppType = {}
+
+function App (props: AppType) {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
         <Header/>
         <Navbar/>
         <div className="app-wrapper-content">
-          <Route path="/dialogs" component={ Dialogs }/>
-          <Route path="/profile" component={ Profile }/>
-          <Route path="/news" component={ News}/>
-          <Route path="/music" component={ Music}/>
-          <Route path="/setting" component={ Setting}/>
+          <Route path="/dialogs" render={ () => <Dialogs/> }/>
+          <Route path="/profile" render={ () => <Profile/> }/>
+          <Route path="/news" render={ () => <News/> }/>
+          <Route path="/music" render={ () => <Music/> }/>
+          <Route path="/setting" render={ () => <Setting/> }/>
         </div>
-
-
       </div>
     </BrowserRouter>
   );
 }
-// https://w7.pngwing.com/pngs/29/684/png-transparent-stormtrooper-boba-fett-star-wars-original-trilogy-helmet-stormtrooper-star-wars-episode-vii-sports-equipment-motorcycle-helmet.png
 
 export default App;
