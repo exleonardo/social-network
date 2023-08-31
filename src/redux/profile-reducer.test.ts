26079aa5833acc7bc2d54c8b1b2e97252d59eeb6
exleonardo <exleonardo@mail.ru>
2023-08-31 23:34:38 +0300

    refactor test

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -1,33 +1,30 @@
-
-import  { ProfilePageType} from "./store";
+import {ProfilePageType} from "./store";
 import profileReducer , {addPostActionCreator , updateNewPostTextCreator} from "./profile-reducer";
 
-    test("message should be added to correct post",()=>{
-        let startState:ProfilePageType = {
-            posts: [
-                { id: '1' , message: 'Hi how are you' , likesCount: '1' } ,
-                { id: '2' , message: 'It\'s my post ' , likesCount: '23' }
-            ] ,
-            newPostText: 'It-kamasutra.com'
-        }
-        const action = addPostActionCreator("test message");
-        const endState = profileReducer(startState, action)
-        expect(endState.posts.length).toEqual(3);
-        expect(endState.posts[endState.posts.length-1].id).toBeDefined()
-        expect(endState.posts[endState.posts.length-1].message).toBe("test message")
-    })
-
-test("newPostText should be added",()=>{
-    let startState:ProfilePageType = {
+let startState: ProfilePageType
+beforeEach ( () => {
+    startState = {
         posts: [
             { id: '1' , message: 'Hi how are you' , likesCount: '1' } ,
             { id: '2' , message: 'It\'s my post ' , likesCount: '23' }
         ] ,
         newPostText: 'It-kamasutra.com'
     }
-    const action = updateNewPostTextCreator("test message");
-    const endState = profileReducer(startState, action)
+} )
+test ( "message should be added to correct post" , () => {
 
-    expect(endState.newPostText).toBe("test message");
-})
+    const action = addPostActionCreator ( "test message" );
+    const endState = profileReducer ( startState , action )
+    expect ( endState.posts.length ).toEqual ( 3 );
+    expect ( endState.posts[endState.posts.length - 1].id ).toBeDefined ()
+    expect ( endState.posts[endState.posts.length - 1].message ).toBe ( "test message" )
+} )
+
+test ( "newPostText should be added" , () => {
+   
+    const action = updateNewPostTextCreator ( "test message" );
+    const endState = profileReducer ( startState , action )
+
+    expect ( endState.newPostText ).toBe ( "test message" );
+} )
 
