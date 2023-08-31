2a74d56f740b83abbaa00aa4f480de5bbe50b2ed
exleonardo <exleonardo@mail.ru>
2023-08-31 23:34:01 +0300

    change Url img

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,12 +3,14 @@ import s from "./ProfileInfo.module.css"
 
 
 const ProfileInfo = () => {
-  return (
-    <div>
-      <div className={s.images}><img src="https://image.wallperz.com/big_thumbs/wallperz.com_big_thumbs_v8ic75mj4xvwjo7k2vsiq03g1rxiix.jpg" alt="ava"/></div>
-      <div className={s.descriptionBlock}>ava + descr</div>
-    </div>
-  );
+    return (
+        <div>
+            <div className={s.images}><img
+                src="https://image.wallperz.com/big_thumbs/wallperz.com_big_thumbs_v8ic75mj4xvwjo7k2vsiq03g1rxiix.jpg"
+                alt="ava"/></div>
+            <div className={s.descriptionBlock}>ava + descr</div>
+        </div>
+    );
 };
 
 export default ProfileInfo;
