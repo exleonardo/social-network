b88464238f7e437c3af2e3f9194a526de103fb74
exleonardo <exleonardo@mail.ru>
2023-10-09 21:32:13 +0300

    89 Lessons - Тесты

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -1,5 +1,6 @@
 import {ProfilePageType} from "./store";
-import profileReducer , {addPostActionCreator} from "./profile-reducer";
+import profileReducer , {addPostActionCreator , getUserProfile , setStatusAC , setUserProfile} from "./profile-reducer";
+import {UsersContactType} from "../API/socialNetworkAPI";
 
 let startState: ProfilePageType
 beforeEach ( () => {
@@ -21,6 +22,39 @@ test ( "message should be added to correct post" , () => {
     expect ( endState.posts[endState.posts.length - 1].id ).toBeDefined ()
     expect ( endState.posts[endState.posts.length - 1].message ).toBe ( "test message" )
 } )
-
+test ( "status should be changed" , () => {
+    const action = setStatusAC ( 'New status' );
+    const endState = profileReducer ( startState , action )
+    expect ( endState.status ).toBeDefined ()
+    expect ( endState.status ).toBe ( "New status" )
+} )
+test ( "users should be added" , () => {
+    const action = setUserProfile ( {
+        aboutMe: "student" ,
+        userId: 1 ,
+        lookingForAJob: true ,
+        lookingForAJobDescription: "Ищу работу Frontend-react-dev" ,
+        fullName: "Alex Khomenok" ,
+        contacts: {
+            github: "https://github.com/exleonardo" ,
+            vk: 'vk.com/exleonardo' ,
+            facebook: "undefined" ,
+            instagram: "@Alexander_Khomenok" ,
+            twitter: "undefined" ,
+            website: "undefined" ,
+            youtube: "undefined" ,
+            mainLink: "undefined" ,
+        } ,
+        photos: {
+            small: "No Photo" ,
+            large: "No Photo"
+        }
+    } );
+    const endState = profileReducer ( startState , action )
+    expect ( endState.profile ).toBeDefined ()
+    expect ( endState.profile?.fullName ).toBe ( "Alex Khomenok" );
+    expect ( endState.profile?.lookingForAJob ).toBeTruthy ();
+    expect ( endState.profile?.userId ).toBeDefined ()
+} )
 
 
