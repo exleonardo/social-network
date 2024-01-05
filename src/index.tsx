db7b3478f7fb839f0dd9dabbf38b1a3fdc1a5ea0
Alexander Khomenok <exleonardo@mail.ru>
2024-01-05 22:36:22 +0300

    fix: import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import store from "./redux/redux-store";
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App} from './App';
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
