46b3cf442fa45692348b926c0c42104b83ec6231
Alexander Khomenok <exleonardo@mail.ru>
2024-01-05 22:29:23 +0300

    refactor: add functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,61 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {Redirect , Route , Switch , withRouter} from 'react-router-dom';
+import {Redirect , Route , Switch} from 'react-router-dom';
 import News from './components/News/News';
 import Setting from './components/Setting/Setting';
 import Music from './components/Music/Music';
-import UsersContainer from "./components/Users/UsersContainer";
+import {UsersPage} from "./components/Users/UsersPage";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/Login";
-import {connect} from "react-redux";
-import {RouteComponentProps} from "react-router";
-import {compose} from "redux";
+import {Login} from "./components/Login/Login";
 import {initializeApp} from "./redux/app-reducer";
-import {AppStateType} from "./redux/redux-store";
+import {useAppDispatch , useAppSelector} from "./redux/redux-store";
 import Preloader from "./components/common/Preloader/Preloader";
 import {withSuspense} from "./hoc/withSuspense";
+import {getInitialized} from "./components/Login/login-selectors";
 
 const DialogsContainer = React.lazy ( () => import("./components/Dialogs/DialogsContainer") )
-const ProfileContainer = React.lazy ( () => import("./components/Profile/ProfileContainer") )
+const ProfileContainer = React.lazy ( () => import("./components/Profile/ProfileMain") )
 
-class App extends React.Component<AppTypeProps> {
-  catchAllUnhandleErrors = () => {
+export const App = () => {
+  const initialized = useAppSelector ( getInitialized )
+  const dispatch = useAppDispatch ()
+  const catchAllUnhandleErrors = () => {
     alert ( "Some Error" )
-
   }
-
-  componentDidMount() {
-    this.props.initializeApp ();
-    window.addEventListener ( 'unhandledrejection' , this.catchAllUnhandleErrors )
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener ( 'unhandledrejection' , this.catchAllUnhandleErrors )
-  }
-
-  render() {
-    if ( !this.props.initialized ) {
-      return <Preloader/>
+  useEffect ( () => {
+    dispatch ( initializeApp () )
+    window.addEventListener ( 'unhandledrejection' , catchAllUnhandleErrors )
+    return () => {
+      window.removeEventListener ( 'unhandledrejection' , catchAllUnhandleErrors )
     }
+  } , [] );
 
 
-    return (
-      <div className="app-wrapper">
-        <HeaderContainer/>
-        <Navbar/>
-        <div className="app-wrapper-content">
-          <Switch>
-            <Route exact path="/"
-                   render={() => <Redirect to={'/profile'}/>}/>
-            <Route path="/dialogs" render={withSuspense ( DialogsContainer )}/>
-            <Route path="/profile/:userId?"
-                   render={withSuspense ( ProfileContainer )}/>
-            <Route path="/users" render={() => <UsersContainer/>}/>
-            <Route path="/login" render={() => <Login/>}/>
-            <Route path="/news" render={() => <News/>}/>
-            <Route path="/music" render={() => <Music/>}/>
-            <Route path="/setting" render={() => <Setting/>}/>
-            <Route path="*" render={() => <div>404 Not Found</div>}/>
-          </Switch>
-        </div>
+  if ( initialized ) {
+    return <Preloader/>
+  }
+
+  return (
+    <div className="app-wrapper">
+      <HeaderContainer/>
+      <Navbar/>
+      <div className="app-wrapper-content">
+        <Switch>
+          <Route exact path="/"
+                 render={() => <Redirect to={'/profile'}/>}/>
+          <Route path="/dialogs" render={withSuspense ( DialogsContainer )}/>
+          <Route path="/profile/:userId?"
+                 render={withSuspense ( ProfileContainer )}/>
+          <Route path="/users" render={() => <UsersPage/>}/>
+          <Route path="/login" render={() => <Login/>}/>
+          <Route path="/news" render={() => <News/>}/>
+          <Route path="/music" render={() => <Music/>}/>
+          <Route path="/setting" render={() => <Setting/>}/>
+          <Route path="*" render={() => <div>404 Not Found</div>}/>
+        </Switch>
       </div>
-    );
-  }
-}
+    </div>
+  );
 
-const mapStateToProps = (state: AppStateType): AppMapStateToProps => {
-  return {
-    initialized: state.app.initialized
-  }
 }
-export default compose<React.ComponentType> (
-  connect ( mapStateToProps , { initializeApp } ) , withRouter ) ( App );
-
-//type
-type AppMapDispatchToProps = {
-  initializeApp: () => void
-}
-export type AppMapStateToProps = {
-  initialized: boolean
-}
-export type AppTypeProps = RouteComponentProps & AppMapDispatchToProps & AppMapStateToProps
