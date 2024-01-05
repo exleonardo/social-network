9fb2d2226144feb62b18cf52302c43ed33a87931
Alexander Khomenok <exleonardo@mail.ru>
2024-01-05 22:37:48 +0300

    feat: add ProfileMain.tsx

---
+++ src/components/Profile/ProfileMain.tsx
@@ -0,0 +1,46 @@
+import React , {useEffect} from "react";
+import Profile from "./Profile";
+import {getStatus , getUserProfile} from "../../redux/profile-reducer";
+import {useAppDispatch , useAppSelector} from "../../redux/redux-store";
+import {RouteComponentProps} from "react-router";
+import {getAuthorizedUserId} from "./profile-selector";
+
+const ProfileMain = ({ match: { params: { userId: userID } } , history }: PropsType) => {
+  const dispatch = useAppDispatch ()
+
+  const authorizedUserId = useAppSelector ( getAuthorizedUserId )
+
+  const refreshProfile = () => {
+
+    let userId = userID;
+
+    if ( !userId ) {
+      userId = !userId ? "29819" : String ( authorizedUserId )
+      if ( !userId ) {
+        history.push ( '/login' )
+      }
+    }
+    if ( !userId ) {
+      console.error ( 'ID should exist in URI params or in state (authorizedUserId)' )
+    }
+    dispatch ( getUserProfile ( userId ) )
+    dispatch ( getStatus ( userId ) )
+  }
+  console.log ( authorizedUserId )
+  useEffect ( () => {
+    refreshProfile ()
+  } , [userID] );
+  return (
+    <div>
+      <Profile isOwner={!userID}/>
+    </div>
+  )
+}
+export default ProfileMain
+
+// //types
+type PropsType = RouteComponentProps<PathParamsType>
+
+type PathParamsType = {
+  userId: string
+}
