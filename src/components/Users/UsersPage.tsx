6a6ac6d3179a7c9b93a3b2d6a8581b5431211daa
Alexander Khomenok <exleonardo@mail.ru>
2024-01-06 17:19:07 +0300

    refactor: del useEffect

--- src/components/Users/UsersPage.tsx
+++ src/components/Users/UsersPage.tsx
@@ -1,21 +1,13 @@
-import {useAppDispatch , useAppSelector} from "../../redux/redux-store";
-import {requestUsers ,} from "../../redux/users-reducer";
-import React , {useEffect} from "react";
+import {useAppSelector} from "../../redux/redux-store";
+import React from "react";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
-import {getCurrentPage , getIsFetching , getPageSize , getUsersFilter} from "./users-selectors";
+import {getIsFetching} from "./users-selectors";
 
 
 export const UsersPage = () => {
 
   const isFetching = useAppSelector ( getIsFetching )
-  const pageSize = useAppSelector ( getPageSize )
-  const currentPage = useAppSelector ( getCurrentPage )
-  const filter = useAppSelector ( getUsersFilter )
-  const dispatch = useAppDispatch ()
-  useEffect ( () => {
-    dispatch ( requestUsers ( currentPage , pageSize , filter ) )
-  } , [] );
 
   return (
     <>
