e50c928b3c331d2e3695909162968d82589dcddc
Alexander Khomenok <exleonardo@mail.ru>
2024-01-06 16:28:24 +0300

    refactor: add hook

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -3,39 +3,47 @@ import {NavLink} from "react-router-dom";
 import userPhoto from "../../assets/images/yoda_star_wars_icon_131348.png";
 
 import {UsersInfoType} from "../../API/profile-api";
+import {useAppDispatch , useAppSelector} from "../../redux/redux-store";
+import {follow , unfollow} from "../../redux/users-reducer";
+import {getFollowingInProgress} from "./users-selectors";
 
 type UserTypeProps = {
-  followingInProgress: Array<number>;
-  follow: (userId: number) => void;
-  unfollow: (userId: number) => void;
   user: UsersInfoType;
 }
-const User = ({ user , followingInProgress , follow , unfollow }: UserTypeProps) => {
+export const User = ({ user }: UserTypeProps) => {
+
+  const followingInProgress = useAppSelector ( getFollowingInProgress )
+  const dispatch = useAppDispatch ()
+
+  const unfollowUser = () => {
+    dispatch ( unfollow ( user.id ) )
+  }
+  const followUser = () => {
+    dispatch ( follow ( user.id ) )
+  }
   return (
     <div>
-                    <span>
-                        <div>
-                            <NavLink to={'/profile/' + user.id}>
-                            <img src={user.photos.small ? user.photos.small : userPhoto} alt="avatar"/></NavLink>
-                        </div>
-                        <div>
-                            {user.followed
-                              ? <button
-                                disabled={followingInProgress.some ( (id) => id === user.id )}
-                                onClick={() => unfollow ( user.id )}>Follow</button>
-                              : <button
-                                disabled={followingInProgress.some ( (id) => id === user.id )}
-                                onClick={() => follow ( user.id )}>Unfollow</button>}
-                        </div>
-                    </span>
+     <span>
+      <div>
+       <NavLink to={'/profile/' + user.id}>
+       <img src={user.photos.small ? user.photos.small : userPhoto} alt="avatar"/></NavLink>
+      </div>
+      <div>
+      {user.followed
+        ? <button
+          disabled={followingInProgress.some ( (id) => id === user.id )}
+          onClick={unfollowUser}>Follow</button>
+        : <button
+          disabled={followingInProgress.some ( (id) => id === user.id )}
+          onClick={followUser}>Unfollow</button>}
+      </div>
+     </span>
       <span>
-                        <span>
-                            <div>{user.name}</div>
-                            <div>{user.status}</div></span>
-                    </span>
+        <span>
+          <div>{user.name}</div>
+          <div>{user.status}</div>
+        </span>
+      </span>
     </div>
   );
 };
-
-
-export default User;
