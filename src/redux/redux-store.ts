a4c58538faac76bba9f1e4d926347b7b9b4ce8df
Alexander Khomenok <exleonardo@mail.ru>
2024-01-06 16:24:59 +0300

    feat: add hook

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -8,15 +8,16 @@ import ThunkMiddleware , {ThunkAction , ThunkDispatch} from "redux-thunk";
 import {reducer as formReducer} from 'redux-form'
 import {FormAction} from "redux-form/lib/actions";
 import appReducer from "./app-reducer";
+import {TypedUseSelectorHook , useDispatch , useSelector} from "react-redux";
 
 export const rootReducer = combineReducers ( {
-    profilePage: profileReducer ,
-    dialogsPage: dialogsReducer ,
-    sidebar: SidebarReducer ,
-    usersPage: usersReducer ,
-    auth: authReducer ,
-    form: formReducer ,
-    app: appReducer
+  profilePage: profileReducer ,
+  dialogsPage: dialogsReducer ,
+  sidebar: SidebarReducer ,
+  usersPage: usersReducer ,
+  auth: authReducer ,
+  form: formReducer ,
+  app: appReducer
 } )
 
 export type AppStateType = ReturnType<typeof rootReducer>
@@ -25,17 +26,19 @@ export type AppStateType = ReturnType<typeof rootReducer>
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore ( rootReducer , /* preloadedState, */ composeEnhancers (
-    applyMiddleware ( ThunkMiddleware )
+  applyMiddleware ( ThunkMiddleware )
 ) );
 
 export type AppActionsType =
-    UserReducerActionType
-    | AuthReducerActionType
-    | ProfileReducerActionType
-    | FormAction
+  UserReducerActionType
+  | AuthReducerActionType
+  | ProfileReducerActionType
+  | FormAction
 export type AppThunk = ThunkAction<Promise<void> , AppStateType , unknown , AppActionsType>
 export type AppDispatchType = ThunkDispatch<AppStateType , any , AnyAction>
-
+export type RootState = ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch: () => AppDispatchType = useDispatch
 export default store
 
 // @ts-ignore
