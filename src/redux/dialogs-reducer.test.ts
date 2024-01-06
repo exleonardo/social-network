21bfde2324f593b1e0fb1578b680da90ecdadafe
Alexander Khomenok <exleonardo@mail.ru>
2024-01-06 16:28:51 +0300

    refactor: import

--- src/redux/dialogs-reducer.test.ts
+++ src/redux/dialogs-reducer.test.ts
@@ -1,16 +1,17 @@
-import state , {DialogsPageType} from "./store";
+import {DialogsPageType} from "./store";
+import state from './redux-store'
 import dialogsReducer , {sendMessageCreator} from "./dialogs-reducer";
 
 test ( "message should be added to correct array" , () => {
-    let startState: DialogsPageType = { ...state.getState ().dialogsPage }
+  let startState: DialogsPageType = { ...state.getState ().dialogsPage }
 
-    const action = sendMessageCreator ( 'test message' );
-    const endState = dialogsReducer ( startState , action )
+  const action = sendMessageCreator ( 'test message' );
+  const endState = dialogsReducer ( startState , action )
 
-    expect ( endState.newMessageBody ).toBe ( 'test message' )
-    expect ( endState.messages.length ).toBe ( 4 )
-    expect ( endState.messages[endState.messages.length - 1].message ).toBeDefined ()
-    expect ( endState.messages[endState.messages.length - 1].id ).toBeDefined ()
+  expect ( endState.newMessageBody ).toBe ( 'test message' )
+  expect ( endState.messages.length ).toBe ( 4 )
+  expect ( endState.messages[endState.messages.length - 1].message ).toBeDefined ()
+  expect ( endState.messages[endState.messages.length - 1].id ).toBeDefined ()
 } )
 
 
