3001963244b199e8bb629577aba97d1d27dc887b
Alexander Khomenok <exleonardo@mail.ru>
2024-01-06 17:18:22 +0300

    feat: add search filter

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,54 +1,76 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import Paginator from "../common/Paginator/Paginator";
-import User from "./User";
-import {UsersInfoType} from "../../API/profile-api";
+import {User} from "./User";
 import {UsersSearchForm} from "./UsersSearchForm";
-import {FormValues} from "../../redux/users-reducer";
-
-type UsersTypeProps = {
-  totalUsersCount: number;
-  pageSize: number;
-  currentPage: number;
-  onPageChanged: (page: number , filter: FormValues) => void;
-  users: UsersInfoType[];
-  follow: (userId: number) => void;
-  unfollow: (userId: number) => void;
-  toggleFollowingProgress: (isFetching: boolean , userId: number) => void
-  followingInProgress: Array<number>
-  onFilterChanged: (filter: FormValues) => void
-  filter: FormValues
-
-}
-const Users = (
-  {
-    totalUsersCount ,
-    pageSize ,
-    currentPage ,
-    onPageChanged ,
-    users ,
-    follow ,
-    unfollow ,
-    toggleFollowingProgress ,
-    followingInProgress ,
-    onFilterChanged ,
-    filter ,
-    ...props
-  }: UsersTypeProps
-) => {
+import {useAppDispatch , useAppSelector} from "../../redux/redux-store";
+import {getCurrentPage , getPageSize , getTotalUsersCount , getUsers , getUsersFilter} from "./users-selectors";
+import {useHistory , useLocation} from "react-router-dom";
+import {requestUsers} from "../../redux/users-reducer";
 
 
+const Users = () => {
+
+  const users = useAppSelector ( getUsers )
+  const filter = useAppSelector ( getUsersFilter )
+  const totalUsersCount = useAppSelector ( getTotalUsersCount )
+  const pageSize = useAppSelector ( getPageSize )
+  const currentPage = useAppSelector ( getCurrentPage )
+  const location = useLocation ();
+  const history = useHistory ()
+  const dispatch = useAppDispatch ()
+
+
+  useEffect ( () => {
+    const parsed = new URLSearchParams ( location.search.substr ( 1 ) )
+    let actualPage = currentPage
+    let actualFilter = filter
+    if ( parsed.get ( 'page' ) ) {
+      actualPage = Number ( parsed.get ( 'page' ) )
+    }
+    if ( parsed.get ( 'term' ) ) {
+      const term = parsed.get ( 'term' ) || ''
+      actualFilter = { ...filter , term }
+    }
+    if ( parsed.get ( 'friend' ) ) {
+      const friend = parsed.get ( 'friend' ) || ''
+      actualFilter = { ...filter , friend }
+    }
+    dispatch ( requestUsers ( actualPage , pageSize , actualFilter ) )
+  } , [] );
+  useEffect ( () => {
+    const params = new URLSearchParams ();
+
+    params.append ( 'term' , filter.term );
+    params.append ( 'friend' , filter.friend );
+    params.append ( 'page' , String ( currentPage ) );
+
+    if ( filter.term === '' ) {
+      params.delete ( 'term' );
+    }
+    if ( filter.friend === '' ) {
+      params.delete ( 'friend' );
+    }
+    if ( currentPage === 1 ) {
+      params.delete ( 'page' );
+    }
+
+
+    history.push ( {
+      pathname: '/users' ,
+      search: `${params.toString ()}`
+    } )
+  } , [filter , currentPage] );
   return (
     <div>
-      <UsersSearchForm onFilterChanged={onFilterChanged}/>
-      <Paginator filter={filter} totalUsersCount={totalUsersCount} pageSize={pageSize} currentPage={currentPage}
-                 onPageChanged={onPageChanged}/>
+      <UsersSearchForm/>
+      <Paginator filter={filter} totalUsersCount={totalUsersCount} pageSize={pageSize} currentPage={currentPage}/>
       <div>
-        {users.map ( (user) => <User key={user.id} followingInProgress={followingInProgress} follow={follow}
-                                     unfollow={unfollow}
-                                     user={user}/>
+        {users.map ( (user) =>
+          <User key={user.id} user={user}/>
         )}
       </div>
     </div>
   );
 };
+
 export default Users;
