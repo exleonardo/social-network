931ccd5cea8aa401662eb17922de577c359a0f8c
Alexander Khomenok <exleonardo@mail.ru>
2024-01-07 23:06:22 +0300

    feat: hide edit status

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,20 +1,49 @@
-import React , {ChangeEvent , useEffect , useState} from 'react';
+import React , {ChangeEvent , memo , useEffect , useState} from 'react';
 import {useAppDispatch , useAppSelector} from "../../../redux/redux-store";
 import {getStatusProfile} from "../profile-selector";
 import {updateStatus} from "../../../redux/profile-reducer";
+import {Button , Input , message} from "antd";
+import {EditOutlined} from "@ant-design/icons";
+import {getUsersId} from "../../Users/users-selectors";
+import {useLocation} from "react-router-dom";
+import {getCurrentUserId} from "../../Login/login-selectors";
 
 
-const ProfileStatusWithHooks = () => {
+const ProfileStatusWithHooks = memo ( () => {
   const statusProfile = useAppSelector ( getStatusProfile )
   const dispatch = useAppDispatch ()
   const [state , setState] = useState ( false );
   const [status , setStatus] = useState ( statusProfile );
+  const [messageApi , contextHolder] = message.useMessage ();
+  const location = useLocation ();
+
+  const userId = useAppSelector ( getUsersId )
+  const currentUserId = useAppSelector ( getCurrentUserId )
+
+  console.log ( userId )
+  console.log ( currentUserId )
+
+
+  const error = (message: string) => {
+    messageApi.open ( {
+      type: 'error' ,
+      content: `${message}` ,
+    } );
+  };
+
+
   useEffect ( () => {
     setStatus ( statusProfile )
   } , [statusProfile] );
+
+
   const activateEditMode = () => {
     setState ( !state )
-    dispatch ( updateStatus ( status ) )
+    dispatch ( updateStatus ( status ) ).catch ( err => {
+      error ( err )
+    } )
+
+
   }
   const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
     if ( e.currentTarget.value.length <= 300 ) {
@@ -24,22 +53,24 @@ const ProfileStatusWithHooks = () => {
 
   return (
     <div style={{ marginTop: '10px' }}>
+      {contextHolder}
       {!state &&
           <div>
               <b>Status:</b> <span
-              onDoubleClick={activateEditMode}>{statusProfile || 'Enter status'}</span>
+          >{statusProfile || 'Enter status'}</span>
+            {userId === currentUserId && <Button onClick={activateEditMode}
+                                                 icon={<EditOutlined rev={undefined}/>}></Button>}
           </div>
       }
       {state &&
           <div>
-              <input onChange={onStatusChange} autoFocus onBlur={activateEditMode}
+              <Input showCount maxLength={300} onChange={onStatusChange} autoFocus onBlur={activateEditMode}
                      value={status}/>
-              <div>{status.length}/300</div>
           </div>
       }
     </div>
   );
 
-}
+} )
 
 export default ProfileStatusWithHooks;
