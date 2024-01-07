1062467586002b56eca156c9debc3591eeefa108
Alexander Khomenok <exleonardo@mail.ru>
2024-01-07 23:08:19 +0300

    feat: add formik

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,40 +1,55 @@
 import React , {memo} from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
-import {PostsTypeProps} from "./MyPostsContainer";
-import {Field , InjectedFormProps , reduxForm} from "redux-form";
-import {maxLengthCreator , required} from "../../../utils/validators";
-import {Textarea} from "../../common/FormsControls/FormsControls";
 
-const maxLength10 = maxLengthCreator ( 10 )
-const MyPosts = memo ( (props: PostsTypeProps) => {
-    const postsElements = props.posts.map ( el => <Post key={el.id} id={el.id} message={el.message}
-                                                        likesCount={el.likesCount}/> )
-    const onAddPost = (values: AddNewPostFormType) => {
-        props.addPost ( values.newPostText );
-    }
-    return (
-        <div className={s.postsBlock}><h3>My Posts</h3>
-            <AddNewPostFormRedux onSubmit={onAddPost}/>
-            <div className={s.posts}>
-                {postsElements}
-            </div>
-        </div>
-    );
+import {Button , Input} from "antd";
+import {useAppDispatch , useAppSelector} from "../../../redux/redux-store";
+import {addPostActionCreator} from "../../../redux/profile-reducer";
+import {getMyPosts , getNewPostText} from "../profile-selector";
+import {useFormik} from "formik";
+
+
+const MyPosts = memo ( () => {
+
+  const { TextArea } = Input;
+  const posts = useAppSelector ( getMyPosts )
+  const dispatch = useAppDispatch ()
+
+  const onAddPost = (value: AddNewPostFormType) => {
+    dispatch ( addPostActionCreator ( value.newPostText ) )
+  }
+  const newPost = useAppSelector ( getNewPostText )
+  const formik = useFormik ( {
+    initialValues: {
+      newPostText: newPost
+    } , onSubmit: onAddPost
+  } )
+
+  const postsElements = posts.map ( el => <Post key={el.id} id={el.id} message={el.message}
+                                                likesCount={el.likesCount}/> )
+
+
+  return (
+    <div className={s.postsBlock}><h3>My Posts</h3>
+      <form onSubmit={formik.handleSubmit}>
+        <TextArea
+          {...formik.getFieldProps ( 'newPostText' )}
+          showCount
+          maxLength={100}
+          placeholder="what's new"
+          style={{ height: 120 , resize: 'none' }}/>
+        <Button htmlType={'submit'}>add post</Button>
+      </form>
+      <div className={s.posts}>
+        {postsElements}
+      </div>
+    </div>
+  );
 } );
 
-export const AddNewPostForm: React.FC<InjectedFormProps<AddNewPostFormType>> = (props) => {
-    return <form onSubmit={props.handleSubmit}>
-        <div><Field placeholder={'Post message'} name={"newPostText"} component={Textarea}
-                    validate={[required , maxLength10]}/></div>
-        <div>
-            <button>add post</button>
-        </div>
-    </form>
-}
-const AddNewPostFormRedux = reduxForm<AddNewPostFormType> ( { form: 'ProfileAddNewPostForm' } ) ( AddNewPostForm )
+
 export default MyPosts;
 //type
 type AddNewPostFormType = {
-    newPostText: string
+  newPostText: string
 }
