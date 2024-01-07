b4aa82ceb913c80d9e893d8e7f91f11a0eb1a4dd
Alexander Khomenok <exleonardo@mail.ru>
2024-01-07 23:01:29 +0300

    feat: add confirm to followUnfollowFlow

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -1,41 +1,50 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import {NavLink , useHistory} from "react-router-dom";
 import userPhoto from "../../assets/images/yoda_star_wars_icon_131348.png";
 
 import {UsersInfoType} from "../../API/profile-api";
-import {useAppDispatch , useAppSelector} from "../../redux/redux-store";
+import {useAppDispatch} from "../../redux/redux-store";
 import {follow , unfollow} from "../../redux/users-reducer";
-import {getFollowingInProgress} from "./users-selectors";
+import {Button , Popconfirm} from "antd";
 
 type UserTypeProps = {
   user: UsersInfoType;
 }
 export const User = ({ user }: UserTypeProps) => {
 
-  const followingInProgress = useAppSelector ( getFollowingInProgress )
-  const dispatch = useAppDispatch ()
 
+  const dispatch = useAppDispatch ()
+  const history = useHistory ()
+  const moveToProfile = () => {
+    history.push ( `profile/${user.id}` )
+  }
   const unfollowUser = () => {
-    dispatch ( unfollow ( user.id ) )
+    return dispatch ( unfollow ( user.id ) )
   }
   const followUser = () => {
-    dispatch ( follow ( user.id ) )
+    return dispatch ( follow ( user.id ) )
   }
   return (
     <div>
      <span>
       <div>
-       <NavLink to={'/profile/' + user.id}>
+       <NavLink onClick={moveToProfile} to={{}}>
        <img src={user.photos.small ? user.photos.small : userPhoto} alt="avatar"/></NavLink>
       </div>
       <div>
       {user.followed
-        ? <button
-          disabled={followingInProgress.some ( (id) => id === user.id )}
-          onClick={unfollowUser}>Follow</button>
-        : <button
-          disabled={followingInProgress.some ( (id) => id === user.id )}
-          onClick={followUser}>Unfollow</button>}
+        ? <Popconfirm
+          title={'Get Exclusive Content Only for Subscribers!'}
+          description="Are you sure you want to subscribe?"
+          onConfirm={unfollowUser}
+        > <Button type={'primary'}
+        >Follow</Button></Popconfirm>
+        : <Popconfirm
+          title={'We\'re sorry that you want to unsubscribe from our updates.'}
+          description="Are you sure you want to unsubscribe"
+          onConfirm={followUser}
+        > <Button
+        >Unfollow</Button></Popconfirm>}
       </div>
      </span>
       <span>
