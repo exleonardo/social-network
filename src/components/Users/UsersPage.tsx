fd7a5b11341d85339c8a5172d7340e580f784fca
Alexander Khomenok <exleonardo@mail.ru>
2024-01-09 01:09:46 +0300

    feat: add Redirect

--- src/components/Users/UsersPage.tsx
+++ src/components/Users/UsersPage.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
 import {getIsFetching} from "./users-selectors";
+import {getIsAuth} from "../Login/login-selectors";
+import {Redirect} from "react-router-dom";
 
 
 export const UsersPage = () => {
 
   const isFetching = useAppSelector ( getIsFetching )
-
+  const initialized = useAppSelector ( getIsAuth )
+  if ( !initialized ) {
+    return <Redirect to={'/unautorized'}/>
+  }
   return (
     <>
       {isFetching ? <Preloader/> : null}
