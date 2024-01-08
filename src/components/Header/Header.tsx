4b8bc2f9bbd30e6212af7a9c340718548ffe7480
Alexander Khomenok <exleonardo@mail.ru>
2024-01-09 01:14:55 +0300

    refactor: Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,46 @@
 import React from "react";
 import s from "./Header.module.css"
 import {NavLink} from "react-router-dom";
+import {useAppDispatch , useAppSelector} from "../../redux/redux-store";
+import {getIsAuth , getLogin} from "../Login/login-selectors";
+import {logOut} from "../../redux/auth-reducer";
+import {Button} from "antd";
+import {MenuFoldOutlined , MenuUnfoldOutlined} from "@ant-design/icons";
 
-type HeaderPropsType = {
-    isAuth: boolean;
-    login: string | null
-    logOut: () => void
+type Header = {
+  collapsed: boolean
+  setCollapsed: (collapsed: boolean) => void
 }
-const Header = (props: HeaderPropsType) => {
-    return (
-        <header className={s.header}>
-            <img
-                src="https://w7.pngwing.com/pngs/501/177/png-transparent-star-wars-the-old-republic-anakin-skywalker-jedi-vs-sith-jedi-order-symbol-logo-monochrome-anakin-skywalker.png"
-                alt="logo"/>
-            <div className={s.loginBlock}>
-                {props.isAuth
-                    ? <div>{props.login}
-                        <button onClick={props.logOut}>Log out</button>
-                    </div>
-                    : <NavLink to={'/login'}>Login</NavLink>}
-            </div>
-        </header>
-    );
+export const Header = ({ collapsed , setCollapsed }: Header) => {
+  const dispatch = useAppDispatch ()
+  const isAuth = useAppSelector ( getIsAuth )
+  const login = useAppSelector ( getLogin )
+
+  const loggedOut = () => {
+    dispatch ( logOut () )
+  }
+  return (
+    <header className={s.header}>
+      <Button
+        className={s.headerButton}
+        type="text"
+        icon={collapsed ? <MenuUnfoldOutlined rev={undefined}/> : <MenuFoldOutlined rev={undefined}/>}
+        onClick={() => setCollapsed ( !collapsed )}
+        style={{
+          fontSize: '16px' ,
+          width: 64 ,
+          height: 64 ,
+        }}
+      />
+      <div className={s.loginBlock}>
+        {isAuth
+          ? <div>
+            {login}
+            <Button onClick={loggedOut}>Log out</Button>
+          </div>
+          : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
+    </header>
+  );
 };
 
-export default Header;
