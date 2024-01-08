94b6298c8c51602811d7c7ca63cba9c240dfae2a
Alexander Khomenok <exleonardo@mail.ru>
2024-01-09 01:15:37 +0300

    refactor: Dialogs.tsx remove DialogsContainer.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,31 +2,50 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import {DialogItem} from './DialogsItem/DialogsItem';
 import {Message} from './Message/Message';
-import {DialogsPropsType} from "./DialogsContainer";
 import AddMessageFormRedux , {FormDataMessageType} from "./AddMessageForm/AddMessageForm";
+import {Redirect} from "react-router-dom";
+import {useAppDispatch , useAppSelector} from "../../redux/redux-store";
+import {getIsAuth} from "../Login/login-selectors";
+import {sendMessageCreator} from "../../redux/dialogs-reducer";
+import {getDialogMessages , getDialogsPage} from "./dialog-selectors";
 
 
-const Dialogs = (props: DialogsPropsType) => {
-    const state = props.dialogsPage
+const Dialogs = () => {
+  const initialized = useAppSelector ( getIsAuth )
 
-    const dialogsElements = state.dialogs.map ( (el , index) => <DialogItem key={index} name={el.name} id={el.id}/> )
+  const dialogsPage = useAppSelector ( getDialogsPage )
+  const dialogsMessages = useAppSelector ( getDialogMessages )
+  const dispatch = useAppDispatch ()
 
-    const messagesElements = state.messages.map ( (el , index) => <Message key={index} message={el.message}
-                                                                           id={el.id}/> )
-    const addNewMessage = (values: FormDataMessageType) => {
-        props.sendMessage ( values.newMessageBody )
-    }
-    return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                {dialogsElements}
-            </div>
-            <div className={s.messages}>
-                <div>{messagesElements}</div>
-                <AddMessageFormRedux onSubmit={addNewMessage}/>
-            </div>
-        </div>
-    );
+
+  if ( !initialized ) {
+    return <Redirect to={'/unautorized'}/>
+  }
+
+
+  const sendMessage = (newMessageBody: string) => {
+    dispatch ( sendMessageCreator ( newMessageBody ) )
+  }
+
+
+  const dialogsElements = dialogsPage.map ( (el , index) => <DialogItem key={index} name={el.name} id={el.id}/> )
+
+  const messagesElements = dialogsMessages.map ( (el , index) => <Message key={index} message={el.message}
+                                                                          id={el.id}/> )
+  const addNewMessage = (values: FormDataMessageType) => {
+    sendMessage ( values.newMessageBody )
+  }
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        {dialogsElements}
+      </div>
+      <div className={s.messages}>
+        <div>{messagesElements}</div>
+        <AddMessageFormRedux onSubmit={addNewMessage}/>
+      </div>
+    </div>
+  );
 };
 
 
