7a6621bcee1bc6ebdf32d5fc4edf475f6a7f53f1
Alexander Khomenok <exleonardo@mail.ru>
2024-01-11 14:32:42 +0300

    feat : migrate to vite

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+// import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
+import { createRoot } from 'react-dom/client'
+
+import { App } from './app/App'
+import store from './redux/redux-store'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+)
