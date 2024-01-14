ff2f6c50a0210d2579902c547436cd5e0026c858
Alexander Khomenok <exleonardo@mail.ru>
2024-01-14 22:46:44 +0300

    feat : add new Preloader.tsx

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,10 +1,20 @@
-import preloader from '../../../assets/images/Settings.gif'
-
-const Preloader = () => {
+import { LoadingOutlined } from '@ant-design/icons'
+import { Spin } from 'antd'
+type PreloaderType = {
+  fullscreen: boolean
+}
+const Preloader = ({ fullscreen }: PreloaderType) => {
   return (
-    <div>
-      <img alt={'ulitka'} src={preloader} />
-    </div>
+    <Spin
+      fullscreen={fullscreen}
+      indicator={<LoadingOutlined spin style={{ fontSize: 40 }} />}
+      style={{
+        left: '50%',
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    />
   )
 }
 
