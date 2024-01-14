467cef5e48fa83ea45041f07c00cda6de0ae4d85
Alexander Khomenok <exleonardo@mail.ru>
2024-01-14 22:40:33 +0300

    feat : add getFollowingInProgress

--- src/components/Users/users-selectors.ts
+++ src/components/Users/users-selectors.ts
@@ -20,9 +20,6 @@ export const getCurrentPage = (state: AppStateType) => {
 export const getIsFetching = (state: AppStateType) => {
   return state.usersPage.isFetching
 }
-export const getFollowingInProgress = (state: AppStateType) => {
-  return state.usersPage.followingInProgress
-}
 export const getUsersFilter = (state: AppStateType) => {
   return state.usersPage.filter
 }
