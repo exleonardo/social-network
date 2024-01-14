aee28e678b7be60a1a0d1904e9ba2988a05ee033
Alexander Khomenok <exleonardo@mail.ru>
2024-01-14 22:51:29 +0300

    feat : add redirect

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,14 +1,17 @@
 import { memo, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
 
 import { AddMessage } from '@/components/Chat/AddMessage'
 import { Messages } from '@/components/Chat/Messages'
 import { getStatus } from '@/components/Chat/chat-selector'
+import { getIsAuth } from '@/components/Login/auth-selectors'
 import { startMessageListening, stopMessageListening } from '@/redux/chat-reducer'
 import { useAppDispatch, useAppSelector } from '@/redux/redux-store'
 
 export const Chat = memo(() => {
   const dispatch = useAppDispatch()
   const status = useAppSelector(getStatus)
+  const isAuth = useAppSelector(getIsAuth)
 
   useEffect(() => {
     dispatch(startMessageListening())
@@ -17,6 +20,9 @@ export const Chat = memo(() => {
       dispatch(stopMessageListening())
     }
   }, [status])
+  if (!isAuth) {
+    return <Redirect to={'/unautorized'} />
+  }
 
   return (
     <div>
