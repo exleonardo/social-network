74e18a30efaf8f059487af1e4c668b81c76875f4
Alexander Khomenok <exleonardo@mail.ru>
2024-01-15 23:59:08 +0300

    refactor : Preloader.tsx

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,19 +1,19 @@
 import { LoadingOutlined } from '@ant-design/icons'
 import { Spin } from 'antd'
+
+import s from './preloader.module.scss'
 type PreloaderType = {
+  content?: boolean
   fullscreen: boolean
+  position?: 'absolute' | 'fixed' | 'relative'
 }
-const Preloader = ({ fullscreen }: PreloaderType) => {
+const Preloader = ({ content, fullscreen, position = 'fixed' }: PreloaderType) => {
   return (
     <Spin
+      className={content ? s.preloaderContent : s.preloader}
       fullscreen={fullscreen}
       indicator={<LoadingOutlined spin style={{ fontSize: 40 }} />}
-      style={{
-        left: '50%',
-        position: 'absolute',
-        top: '50%',
-        transform: 'translate(-50%, -50%)',
-      }}
+      style={{ position: position }}
     />
   )
 }
