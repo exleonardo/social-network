105a3242f1baab2831b6d933c2ed00cb9211eadd
Alexander Khomenok <exleonardo@mail.ru>
2024-01-18 22:28:45 +0300

    feat : add Icon.tsx

---
+++ src/shared/Icon/Icon.tsx
@@ -0,0 +1,16 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import icon from './icons.svg'
+type IconProps = {
+  className?: string
+  name: string
+  size?: string
+} & ComponentPropsWithoutRef<'svg'>
+
+export const Icon = ({ className, name, size = '16px', ...rest }: IconProps) => {
+  return (
+    <svg className={className} height={size} width={size} {...rest}>
+      <use href={`${icon}#${name}`} />
+    </svg>
+  )
+}
