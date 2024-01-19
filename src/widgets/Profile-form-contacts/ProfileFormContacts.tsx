ab134dd8ad9980de0e7b374912c817a6c23faf2f
Alexander Khomenok <exleonardo@mail.ru>
2024-01-19 17:20:21 +0300

    feat: add new component

---
+++ src/widgets/Profile-form-contacts/ProfileFormContacts.tsx
@@ -0,0 +1,34 @@
+import { useAppSelector } from '@/app/redux-store'
+import { getProfile } from '@/components/profile-selector'
+import { ErrorTitle } from '@/entities/Error-title/ErrorTitle'
+import { Input } from 'antd'
+import { FormikValues } from 'formik'
+
+import s from './profile-form-contacts.module.scss'
+
+type ContactsType = {
+  formik: FormikValues
+}
+export const ProfileFormContacts = ({ formik }: ContactsType) => {
+  const profile = useAppSelector(getProfile)
+
+  if (!profile) {
+    return
+  }
+  const contacts = Object.keys(profile.contacts)
+
+  return (
+    <div>
+      <div className={s.contactTitle}>Contacts:</div>
+      {contacts.map(el => {
+        return (
+          <div>
+            <b>{el}</b>
+            <Input status={formik.errors[el] && 'error'} {...formik.getFieldProps(el)} />
+            {formik.errors[el] && <ErrorTitle title={formik.errors[el]} />}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
