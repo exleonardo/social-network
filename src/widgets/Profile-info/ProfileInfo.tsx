4b5131add850e57d60229da13ee682b19f191512
Alexander Khomenok <exleonardo@mail.ru>
2024-01-19 17:22:08 +0300

    feat: add new component

---
+++ src/widgets/Profile-info/ProfileInfo.tsx
@@ -0,0 +1,11 @@
+import Card from '@/entities/Card/Card'
+
+import s from './profile-Info.module.scss'
+
+export const ProfileInfo = () => {
+  return (
+    <div className={s.descriptionBlock}>
+      <Card />
+    </div>
+  )
+}
