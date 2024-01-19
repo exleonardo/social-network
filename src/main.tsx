111966e4e67b0b29030400ce1f3065e9f9434cf5
Alexander Khomenok <exleonardo@mail.ru>
2024-01-19 17:27:19 +0300

    refactor: import

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 
 import { createRoot } from 'react-dom/client'
 
-import { App } from './app/App'
-import store from './redux/redux-store'
+import App from './app/App'
+import store from './app/redux-store'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
