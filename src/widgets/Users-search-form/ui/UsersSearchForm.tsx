51db9678fdd88d747b7c2a381a31c29b5eb2937b
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:11:01 +0300

    feat: add customHook

--- src/widgets/Users-search-form/ui/UsersSearchForm.tsx
+++ src/widgets/Users-search-form/ui/UsersSearchForm.tsx
@@ -1,39 +1,13 @@
 import { memo } from 'react'
 
-import { useAppDispatch, useAppSelector } from '@/app/store/redux-store'
 import { FindUser } from '@/features/Find-user/ui/FindUser'
-import { UserSubscriptionSelect } from '@/features/User-subscription-select/UserSubscriptionSelect'
-import { getPageSize, getUsersFilter } from '@/pages/Users/selectors/users-selectors'
-import { FormValues, requestUsers } from '@/redux/users-reducer'
-import { useFormik } from 'formik'
-import { FormikHelpers } from 'formik/dist/types'
+import { UserSubscriptionSelect } from '@/features/User-subscription-select/ui/UserSubscriptionSelect'
+import { useUserSearchForm } from '@/widgets/Users-search-form/hooks/useUserSearchForm'
 
 import s from '../style/index.module.scss'
 
 export const UsersSearchForm = memo(() => {
-  const dispatch = useAppDispatch()
-  const pageSize = useAppSelector(getPageSize)
-  const filter = useAppSelector(getUsersFilter)
-  const submit = (value: FormValues, { setSubmitting }: FormikHelpers<FormValues>) => {
-    const filterValue = { friend: filter.friend, term: value.term }
-
-    onFilterChanged(filterValue)
-    setSubmitting(false)
-  }
-  const formik = useFormik({
-    initialValues: {
-      friend: filter.friend,
-      term: filter.term,
-    },
-    onSubmit: submit,
-  })
-  const onFilterChanged = (filter: FormValues) => {
-    dispatch(requestUsers(1, pageSize, filter))
-  }
-
-  const onSearch = () => {
-    formik.submitForm()
-  }
+  const { formik, onSearch } = useUserSearchForm()
 
   return (
     <form onSubmit={formik.handleSubmit}>
