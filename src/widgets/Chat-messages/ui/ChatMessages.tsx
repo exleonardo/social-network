207f4e63544a9763f816fb366994c8b871a5c9f8
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:10:54 +0300

    feat: add customHook

--- src/widgets/Chat-messages/ui/ChatMessages.tsx
+++ src/widgets/Chat-messages/ui/ChatMessages.tsx
@@ -1,31 +1,12 @@
-import React, { memo, useEffect, useRef, useState } from 'react'
+import { memo } from 'react'
 
-import { useAppSelector } from '@/app/store/redux-store'
-import { getMessages } from '@/pages/Chat/selectors/chat-selector'
 import { ChatMessage } from '@/widgets/Chat-message/ui/ChatMessage'
+import { useChatMessages } from '@/widgets/Chat-messages/hooks/useChatMessages'
 
 import s from '../style/index.module.scss'
+
 export const ChatMessages = memo(() => {
-  const messages = useAppSelector(getMessages)
-  const messagesAmchorRef = useRef<HTMLDivElement>(null)
-  const [isAutoScroll, setIsAutoScroll] = useState(true)
-  const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
-    const element = e.currentTarget
-
-    if (Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) < 350) {
-      !isAutoScroll && setIsAutoScroll(true)
-    } else {
-      isAutoScroll && setIsAutoScroll(false)
-    }
-  }
-
-  useEffect(() => {
-    if (isAutoScroll) {
-      messagesAmchorRef.current?.scrollIntoView({ behavior: 'smooth' })
-    }
-
-    return () => {}
-  }, [isAutoScroll, messages])
+  const { messages, messagesAmchorRef, scrollHandler } = useChatMessages()
 
   return (
     <div className={s.chatMessages} onScroll={scrollHandler}>
