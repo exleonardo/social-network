2e01f8e8520c5811297d4e982a2bad2a709b41d5
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:10:09 +0300

    feat: add customHook

--- src/widgets/Status/ui/Status.tsx
+++ src/widgets/Status/ui/Status.tsx
@@ -1,56 +1,25 @@
-import { ChangeEvent, memo, useEffect, useState } from 'react'
+import { memo } from 'react'
 
-import { useAppDispatch, useAppSelector } from '@/app/store/redux-store'
-import { getCurrentUserId } from '@/pages/Login/selectors/auth-selectors'
-import { getStatusProfile } from '@/pages/Profile/selectors/profile-selector'
-import { getUsersId } from '@/pages/Users/selectors/users-selectors'
-import { updateStatus } from '@/redux/profile-reducer'
-import { Input, Modal, message } from 'antd'
+import { useStatus } from '@/widgets/Status/hooks/useStatus'
+import { Input, Modal } from 'antd'
 
 import s from '../style/index.module.scss'
+
 type StatusType = {
   className?: string
 }
 export const Status = memo(({ className }: StatusType) => {
-  const statusProfile = useAppSelector(getStatusProfile)
-  const dispatch = useAppDispatch()
-  const [state, setState] = useState(false)
-  const [status, setStatus] = useState(statusProfile)
-  const [messageApi, contextHolder] = message.useMessage()
-
-  const userId = useAppSelector(getUsersId)
-  const currentUserId = useAppSelector(getCurrentUserId)
-
-  const error = (message: string) => {
-    messageApi.open({
-      content: `${message}`,
-      type: 'error',
-    })
-  }
-
-  useEffect(() => {
-    setStatus(statusProfile)
-  }, [statusProfile])
-  const cancel = () => {
-    if (userId === currentUserId) {
-      setState(!state)
-    }
-  }
-
-  const activateEditMode = () => {
-    dispatch(updateStatus(status))
-      .then(() => {
-        setState(!state)
-      })
-      .catch(err => {
-        error(err)
-      })
-  }
-  const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.currentTarget.value.length <= 300) {
-      setStatus(e.currentTarget.value)
-    }
-  }
+  const {
+    activateEditMode,
+    cancel,
+    contextHolder,
+    currentUserId,
+    onStatusChange,
+    state,
+    status,
+    statusProfile,
+    userId,
+  } = useStatus()
 
   return (
     <div className={className ? className : s.status} onClick={cancel}>
