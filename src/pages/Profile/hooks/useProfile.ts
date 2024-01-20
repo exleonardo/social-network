7ed68a9fbf9dab1c35c5a2e4f12b3433c876eac3
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:05:02 +0300

    feat: add hook

--- src/pages/Profile/hooks/useProfile.ts
+++ src/pages/Profile/hooks/useProfile.ts
@@ -1,13 +1,32 @@
-import { useAppSelector } from '@/app/store/redux-store'
+import { useEffect } from 'react'
+
+import { useAppDispatch, useAppSelector } from '@/app/store/redux-store'
 import { getCurrentUserId, getIsAuth } from '@/pages/Login/selectors/auth-selectors'
 import { getProfile } from '@/pages/Profile/selectors/profile-selector'
 import { getIsFetching } from '@/pages/Users/selectors/users-selectors'
+import { getStatus, getUserProfile } from '@/redux/profile-reducer'
 
-export const useProfile = () => {
+export const useProfile = (userId: string) => {
+  const dispatch = useAppDispatch()
   const authorizedUserId = useAppSelector(getCurrentUserId)
   const isAuth = useAppSelector(getIsAuth)
   const isFetching = useAppSelector(getIsFetching)
   const profile = useAppSelector(getProfile)
+
+  if (!userId) {
+    userId = String(authorizedUserId)
+  }
+
+  const refreshProfile = () => {
+    if (isAuth) {
+      dispatch(getUserProfile(userId))
+      dispatch(getStatus(userId))
+    }
+  }
+
+  useEffect(() => {
+    refreshProfile()
+  }, [isAuth, userId])
 
   return { authorizedUserId, isAuth, isFetching, profile }
 }
