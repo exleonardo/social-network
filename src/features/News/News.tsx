e4eae8db93ad709d2cf8b83269753c42009b7009
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 16:45:14 +0300

    refactor: move to new folder

--- src/features/News/News.tsx
+++
@@ -1,7 +0,0 @@
-import s from './news.module.scss'
-
-const News = () => {
-  return <div className={s.news}>news</div>
-}
-
-export default News
---
+++ src/features/News/ui/News.tsx
@@ -0,0 +1,5 @@
+import s from '../style/index.module.scss'
+
+export const News = () => {
+  return <div className={s.news}>Be later....</div>
+}
