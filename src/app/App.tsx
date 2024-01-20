681e9bc9f5865a4f3f796084608dea29816a450e
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:08:33 +0300

    refactor: import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
 import { useEffect } from 'react'
 
 import { withProviders } from '@/app/providers/withProviders'
-import { useAppDispatch, useAppSelector } from '@/app/redux-store'
-import Preloader from '@/features/Preloader/Preloader'
+import { useAppDispatch, useAppSelector } from '@/app/store/redux-store'
+import { Preloader } from '@/features/Preloader/ui/Preloader'
 import { Routing } from '@/pages/Routing'
 import { initializeApp } from '@/redux/app-reducer'
-import { FooterContent } from '@/widgets/Footer/Footer'
-import { Header } from '@/widgets/Header/Header'
-import { Navigate } from '@/widgets/Navigate/Navigate'
+import { FooterContent } from '@/widgets/Footer/ui/Footer'
+import { Header } from '@/widgets/Header'
+import { Navigate } from '@/widgets/Navigate/ui/Navigate'
 import { Layout } from 'antd'
 
-import './app.scss'
+import './styles/index.scss'
 
 const App = () => {
   const initialized = useAppSelector(state => state.app.initialized)
