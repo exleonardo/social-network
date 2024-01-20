af69048cf9664652c4b510649479ac1bbc91660d
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:09:26 +0300

    feat: add customHook

--- src/pages/Login/ui/Login.tsx
+++ src/pages/Login/ui/Login.tsx
@@ -1,43 +1,12 @@
 import { Redirect } from 'react-router-dom'
 
-import { useAppDispatch, useAppSelector } from '@/app/store/redux-store'
-import { getCaptchaUrl, getIsAuth } from '@/pages/Login/selectors/auth-selectors'
-import { clearCaptchaUrl, login } from '@/redux/auth-reducer'
+import { useLogin } from '@/pages/Login/hooks/useLogin'
 import { Checkbox } from 'antd'
-import { useFormik } from 'formik'
 
 import s from '../style/index.module.scss'
 
 export const Login = () => {
-  const isAuth = useAppSelector(getIsAuth)
-  const dispatch = useAppDispatch()
-
-  const captcha = useAppSelector(getCaptchaUrl)
-
-  const formik = useFormik({
-    initialValues: {
-      captcha: '',
-      email: '',
-      password: '',
-      rememberMe: false,
-    },
-    onSubmit: (formData, formikHelpers) => {
-      formikHelpers.setSubmitting(true)
-      dispatch(login(formData.email, formData.password, formData.rememberMe, formData.captcha))
-        .then(res => {
-          if (res.length) {
-            formik.setFieldError('email', res)
-          } else if (res.login) {
-            /* empty */
-          }
-          formik.setFieldError(res.field, res.error)
-        })
-        .finally(() => {
-          formikHelpers.setSubmitting(false)
-          dispatch(clearCaptchaUrl())
-        })
-    },
-  })
+  const { captcha, formik, isAuth } = useLogin()
 
   if (isAuth) {
     return <Redirect to={'/'} />
