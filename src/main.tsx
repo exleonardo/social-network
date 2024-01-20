2c2546099da3ed457ab2076b16da41ac94350c55
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:05:32 +0300

    refactor: import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 
 import App from './app/App'
-import store from './app/redux-store'
+import store from './app/store/redux-store'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
