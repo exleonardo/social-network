2f52682d87331586c46daf3eca6e5a92011c07e5
Alexander Khomenok <exleonardo@mail.ru>
2024-01-20 17:10:47 +0300

    feat: add customHook

--- src/features/Edit-profile-button/ui/EditProfileButton.tsx
+++ src/features/Edit-profile-button/ui/EditProfileButton.tsx
@@ -1,25 +1,24 @@
-import { useAppDispatch, useAppSelector } from '@/app/store/redux-store'
-import { getCurrentUserId } from '@/pages/Login/selectors/auth-selectors'
-import { getEditProfile, getProfile } from '@/pages/Profile/selectors/profile-selector'
+import { useAppDispatch } from '@/app/store/redux-store'
+import { useEditProfile } from '@/features/Edit-profile-button/hooks/useEditProfile'
 import { setEditProfile } from '@/redux/profile-reducer'
+import { Button } from '@/shared/Button'
 
 type EditProfileButtonType = {
   className?: string
 }
 export const EditProfileButton = ({ className }: EditProfileButtonType) => {
-  const profile = useAppSelector(getProfile)
-  const authorizedUserId = useAppSelector(getCurrentUserId)
+  const { authorizedUserId, editProfile, profile } = useEditProfile()
   const dispatch = useAppDispatch()
-  const editProfile = useAppSelector(getEditProfile)
+
   const goToEditMode = () => {
     dispatch(setEditProfile(!editProfile))
   }
 
   return (
     authorizedUserId === profile?.userId && (
-      <button className={className} onClick={goToEditMode}>
+      <Button className={className} onClick={goToEditMode}>
         Edit
-      </button>
+      </Button>
     )
   )
 }
