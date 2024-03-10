bd5089d54a87ba45652a0cdd71ee0ce97395f774
Alexander Khomenok <exleonardo@mail.ru>
2024-03-10 14:56:41 +0300

    style:add responsive

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { withProviders } from '@/app/providers/withProviders'
 import { useAppDispatch, useAppSelector } from '@/app/store/redux-store'
+import { Navbar } from '@/features/Navbar'
 import { Preloader } from '@/features/Preloader/ui/Preloader'
+import { getCollapsed } from '@/pages/Profile/selectors/profile-selector'
 import { Routing } from '@/pages/Routing'
 import { initializeApp } from '@/redux/app-reducer'
 import { FooterContent } from '@/widgets/Footer/ui/Footer'
@@ -15,6 +17,11 @@ import './styles/index.scss'
 const App = () => {
   const initialized = useAppSelector(state => state.app.initialized)
   const dispatch = useAppDispatch()
+  const [isTable, setIsMobile] = useState(window.innerWidth <= 992)
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 992)
+  }
+  const collapsed = useAppSelector(getCollapsed)
 
   useEffect(() => {
     dispatch(initializeApp())
@@ -24,11 +31,14 @@ const App = () => {
     return <Preloader fullscreen />
   }
 
+  window.addEventListener('resize', handleResize)
+
   return (
     <Layout className={'layout'}>
-      <Navigate />
+      {!isTable && <Navigate />}
       <Layout style={{ background: 'inherit', border: 'none' }}>
         <Header />
+        {isTable && collapsed && <Navbar />}
         <Routing />
         <FooterContent />
       </Layout>
