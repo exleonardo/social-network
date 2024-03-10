f17b5ca8a487ba4ccc0938d4766241062e933796
Alexander Khomenok <exleonardo@mail.ru>
2024-03-10 16:43:26 +0300

    style: add clsx

--- src/features/Find-user/ui/FindUser.tsx
+++ src/features/Find-user/ui/FindUser.tsx
@@ -1,17 +1,22 @@
 import Search from 'antd/es/input/Search'
+import { clsx } from 'clsx'
 
+import s from '../style/index.module.scss'
 type FindUserType = {
+  className?: string
   onSearch: () => void
 }
-export const FindUser = ({ onSearch, ...props }: FindUserType) => {
+export const FindUser = ({ className, onSearch, ...props }: FindUserType) => {
+  const classes = clsx(s.findUser, className)
+
   return (
     <Search
       allowClear
+      className={classes}
       enterButton={'Search'}
       onSearch={onSearch}
       placeholder={'Find user'}
       size={'large'}
-      style={{ width: '300px' }}
       {...props}
     />
   )
