0a9ad043b633177cd305dac0ead09481a471e4c0
Alexander Khomenok <exleonardo@mail.ru>
2024-03-10 14:55:33 +0300

    style:add clsx

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,11 +1,14 @@
 import { LoggetInUser } from '@/features/Logget-in-user/ui/LoggetInUser'
 import { ToggleMenu } from '@/features/Toggle-navbar/ui/ToggleMenu'
+import { clsx } from 'clsx'
 
 import s from '../style/index.module.scss'
 
 export const Header = () => {
+  const classes = clsx(s.header)
+
   return (
-    <header className={s.header}>
+    <header className={classes}>
       <ToggleMenu />
       <LoggetInUser />
     </header>
