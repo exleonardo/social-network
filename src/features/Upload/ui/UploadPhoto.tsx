ba6229581a052631b89ce137d5307eacaa28efa6
Alexander Khomenok <exleonardo@mail.ru>
2024-03-10 14:52:02 +0300

    feat:change style add useRef

--- src/features/Upload/ui/UploadPhoto.tsx
+++ src/features/Upload/ui/UploadPhoto.tsx
@@ -1,26 +1,34 @@
+import { useRef } from 'react'
+
 import { instance, setting } from '@/API/api'
 import { useUploadPhoto } from '@/features/Upload/hook/useUploadPhoto'
 import { Button } from '@/shared/Button'
 import { Upload } from 'antd'
+import { clsx } from 'clsx'
 
 type UploadFotoType = {
   className?: string
 }
 export const UploadPhoto = ({ className }: UploadFotoType) => {
   const { authorizedUserId, props, userId } = useUploadPhoto()
+  const classes = clsx(className)
+  const buttonRef = useRef<HTMLButtonElement>(null)
 
   return (
     authorizedUserId === userId && (
-      <Upload
-        headers={setting.headers}
-        showUploadList={false}
-        style={{ display: 'block' }}
-        withCredentials
-        {...props}
-        action={`${instance.defaults.baseURL}/profile/photo`}
-      >
-        <Button className={className}>Upload Photo</Button>
-      </Upload>
+      <Button className={classes} onClick={() => buttonRef?.current?.click()}>
+        Upload Photo
+        <Upload
+          headers={setting.headers}
+          showUploadList={false}
+          style={{ display: 'block' }}
+          withCredentials
+          {...props}
+          action={`${instance.defaults.baseURL}/profile/photo`}
+        >
+          <button hidden ref={buttonRef} />
+        </Upload>
+      </Button>
     )
   )
 }
